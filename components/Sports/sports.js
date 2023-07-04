71007c3c8edaeeca36158d287734c749e085f812
Nayan Bagale <nvbagale@gmail.com>
2023-07-04 18:54:57 +0530

    git push

---
+++ components/Sports/sports.js
@@ -0,0 +1,101 @@
+import Image from "next/image";
+// import badIcon from '../../public/badminton-icon.svg';
+
+function sports() {
+  return (
+    <section className=" min-h-screen w-full bg-[#009990]">
+      <div className=" flex items-center min-h-screen relative">
+        <div>
+          <div className=" relative bg-white w-[38.63px] h-[38.63px] rounded-full mb-4 ml-2">
+            <Image
+              src={"/badminton-icon.svg"}
+              width={24.03}
+              height={39.65}
+              className=" absolute right-[-0.70rem] top-[-0.1rem] "
+            />
+          </div>
+          <div className=" relative bg-white w-[38.63px] h-[38.63px] rounded-full mb-4 ml-2">
+            <Image
+              src={"/gym-icon.svg"}
+              width={22.87}
+              height={39.5}
+              className=" absolute  right-[-0.70rem]"
+            />
+          </div>
+          <div className=" relative bg-white w-[38.63px] h-[38.63px] rounded-full mb-4 ml-2">
+            <Image
+              src={"/swiming-icon.svg"}
+              width={38.54}
+              height={19.2}
+              className=" absolute -right-4 top-[0.75rem]"
+            />
+          </div>
+          <div className=" relative bg-white w-[38.63px] h-[38.63px] rounded-full mb-4 ml-2">
+            <Image
+              src={"/tennis-icon.svg"}
+              width={31.3}
+              height={38.13}
+              className=" absolute right-[-0.85rem]"
+            />
+          </div>
+        </div>
+        <div className=" w-[304px] h-[371.56px] bg-white rounded-[8px] mx-16 flex flex-col items-center justify-center">
+          <div className=" left-[-2rem] top-[-3.5rem] relative bg-[#F1F8FF] rounded-full w-[112.59px] h-[112.59px]">
+            <Image
+              src={"/badminton-small.svg"}
+              width={70.06}
+              height={115.56}
+              className=" absolute right-[-2rem] top-[-0.4rem]"
+            />
+          </div>
+          <div className=" text-[#1F1F1F] font-bold text-2xl">BADMINTON</div>
+        </div>
+        <div className=" w-[28.688rem] gap-2 flex flex-col">
+          <h1 className=" text-4xl font-bold">
+            Empowering Badminton Excellence
+          </h1>
+          <p>
+            With 40 full-time students residing and training in Kanha, they will
+            receive top-notch coaching from the Pullela Gopichand Badminton
+            Academy. Our international-quality facility boasts 14 courts, a
+            100-person viewing gallery, and the capacity to host tournaments at
+            all levels. Join us as we nurture the talent of aspiring badminton
+            players.
+          </p>
+        </div>
+        <Image
+          src={"/shuttel.svg"}
+          width={69}
+          height={69.62}
+          className=" absolute top-[12%] left-[28%] "
+        />
+        <Image
+          src={"/shuttel.svg"}
+          width={69}
+          height={69.62}
+          className=" absolute bottom-[30%] right-[20%] rotate-90"
+        />
+        <Image
+          src={"/badminton-img1.jpg"}
+          width={240}
+          height={170}
+          className=" absolute top-0 right-[12%]"
+        />
+        <Image
+          src={"/badminton-img2.jpg"}
+          width={290}
+          height={553}
+          className=" absolute right-0 z-10"
+        />
+        <Image
+          src={"/badminton-img3.jpg"}
+          width={240}
+          height={170}
+          className=" absolute bottom-0 right-[15%] z-20"
+        />
+      </div>
+    </section>
+  );
+}
+
+export default sports
