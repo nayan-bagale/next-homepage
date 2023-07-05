59cc6df4ca23f7663dc6c09851c43b07a5d0a23b
Nayan Bagale <nvbagale@gmail.com>
2023-07-06 00:35:00 +0530

    git push

--- components/Sports/Template.js
+++ components/Sports/Template.js
@@ -1,16 +1,16 @@
 import Image from "next/image";
 
-function Template({ data }) {
+function Template({ data, all_data }) {
   return (
     <div className=" ml-32 flex items-center min-h-screen relative">
       {/* Middle Cards */}
       <div className=" z-20 w-[304px] h-[371.56px] bg-white rounded-[8px] mx-16 flex flex-col items-center justify-center">
         <div className=" left-[-2rem] top-[-2.5rem] relative bg-[#F1F8FF] rounded-full w-[112.59px] h-[112.59px]">
           <Image
-            src={data.svg1}
-            width={data.svg1_width}
+            src={data.svg[1].img}
+            width={data.svg[1].width}
             height={115.56}
-            className={" absolute " + data.svg1_pos}
+            className={" absolute " + data.svg[1].position}
             alt="svg"
           />
         </div>
@@ -27,14 +27,14 @@ function Template({ data }) {
 
       {/* Two flooted SVGs */}
       <Image
-        src={data.svg2}
+        src={data.svg[2].img}
         width={69.0}
         height={69.0}
         className=" absolute top-[20%] left-[20%] w-auto h-auto "
         alt="svg1"
       />
       <Image
-        src={data.svg2}
+        src={data.svg[2].img}
         width={69.0}
         height={69.0}
         className=" absolute bottom-[28%] right-[23%] w-auto h-auto -scale-x-100"
@@ -44,21 +44,21 @@ function Template({ data }) {
 
       {/* Sports Images */}
       <Image
-        src={data.img1}
+        src={data.img[1]}
         width={343}
         height={248}
         className=" absolute top-0 right-[12%]"
         alt="img1"
       />
       <Image
-        src={data.img2}
+        src={data.img[2]}
         width={359}
         height={753}
         className=" absolute right-0 z-10"
         alt="img2"
       />
       <Image
-        src={data.img3}
+        src={data.img[3]}
         width={270}
         height={250}
         className=" absolute bottom-0 right-[18%] z-20"
@@ -67,30 +67,40 @@ function Template({ data }) {
       {/* End Sports Images */}
 
       {/* Two blur cards flooting up & down */}
-      <div className=" z-10 bottom-[2rem] left-1 absolute scale-[.70] opacity-50 blur w-[304px] h-[371.56px] bg-white rounded-[8px] mx-16 flex flex-col items-center justify-center">
-        <div className=" left-[-2rem] top-[-3.5rem] relative bg-[#F1F8FF] rounded-full w-[112.59px] h-[112.59px]">
-          <Image
-            src={"/badminton-small.svg"}
-            width={70.06}
-            height={115.56}
-            className=" absolute right-[-2rem] top-[-0.4rem]"
-            alt="blur img"
-          />
-        </div>
-        <div className=" text-[#1F1F1F] font-bold text-2xl">BADMINTON</div>
-      </div>
       <div className=" z-10 top-[2rem] left-1 absolute scale-[.70] opacity-50 blur w-[304px] h-[371.56px] bg-white rounded-[8px] mx-16 flex flex-col items-center justify-center">
         <div className=" left-[-2rem] top-[-3.5rem] relative bg-[#F1F8FF] rounded-full w-[112.59px] h-[112.59px]">
           <Image
-            src={"/badminton-small.svg"}
-            width={70.06}
+            src={all_data[data.blur_cards.up].svg[1].img}
+            width={all_data[data.blur_cards.up].svg[1].width}
             height={115.56}
-            className=" absolute right-[-2rem] top-[-0.4rem]"
+            className={
+              " absolute " + all_data[data.blur_cards.up].svg[1].position
+            }
             alt="blur img"
           />
         </div>
-        <div className=" text-[#1F1F1F] font-bold text-2xl">BADMINTON</div>
+        <div className=" text-[#1F1F1F] font-bold text-2xl">
+          {all_data[data.blur_cards.up].h2}
+        </div>
       </div>
+
+      <div className=" z-10 bottom-[2rem] left-1 absolute scale-[.70] opacity-50 blur w-[304px] h-[371.56px] bg-white rounded-[8px] mx-16 flex flex-col items-center justify-center">
+        <div className=" left-[-2rem] top-[-3.5rem] relative bg-[#F1F8FF] rounded-full w-[112.59px] h-[112.59px]">
+          <Image
+            src={all_data[data.blur_cards.down].svg[1].img}
+            width={all_data[data.blur_cards.down].svg[1].width}
+            height={115.56}
+            className={
+              " absolute " + all_data[data.blur_cards.down].svg[1].position
+            }
+            alt="blur img"
+          />
+        </div>
+        <div className=" text-[#1F1F1F] font-bold text-2xl">
+          {all_data[data.blur_cards.down].h2}
+        </div>
+      </div>
+
       {/* End */}
     </div>
   );
--- components/Sports/page.js
+++ components/Sports/page.js
@@ -12,53 +12,101 @@ function Page() {
       color: "#009990",
       h1: "EMPOWERING BADMINTON EXCELLENCE",
       p: `With 40 full-time students residing and training in Kanha, they will receive top-notch coaching from the Pullela Gopichand Badminton Academy. Our international-quality facility boasts 14 courts, a 100-person viewing gallery, and the capacity to host tournaments at all levels. Join us as we nurture the talent of aspiring badminton players.`,
-      img1: "/badminton-img1.jpg",
-      img2: "/badminton-img2.jpg",
-      img3: "/badminton-img3.jpg",
-      svg1: "/badminton-small.svg",
-      svg1_width: 70.06,
-      svg1_pos: "right-[-2rem] top-[-0.4rem]",
       h2: "BADMINTON",
-      svg2: "/shuttel.svg",
+      img: {
+        1: "/badminton-img1.jpg",
+        2: "/badminton-img2.jpg",
+        3: "/badminton-img3.jpg",
+      },
+      svg: {
+        1: {
+          img: "/badminton-small.svg",
+          width: 70.06,
+          position: "right-[-2rem] top-[-0.4rem]",
+        },
+        2: {
+          img: "/shuttel.svg",
+        },
+      },
+      blur_cards: {
+        up: "squash",
+        down: "gym",
+      },
     },
     gym: {
       color: "#474747",
       h1: "FITNESS OASIS AWAITS",
       p: `We have a fully equipped gym where you can begin or continue your fitness journey. There are numerous benefits to joining a gym and incorporating regular exercise into your routine. Overall, the benefits of joining a gym go beyond physical health and can positively impact mental and emotional wellbeing as well`,
-      img1: "/gym-img1.jpg",
-      img2: "/gym-img2.jpg",
-      img3: "/gym-img3.jpg",
-      svg1: "/gym-small.svg",
-      svg1_width: 70.06,
-      svg1_pos: "right-[-2rem] top-[-0.4rem]",
       h2: "GYM",
-      svg2: "/dumbell.svg",
+      img: {
+        1: "/gym-img1.jpg",
+        2: "/gym-img2.jpg",
+        3: "/gym-img3.jpg",
+      },
+      svg: {
+        1: {
+          img: "/gym-small.svg",
+          width: 70.06,
+          position: "right-[-2rem] top-[-0.4rem]",
+        },
+        2: {
+          img: "/dumbell.svg",
+        },
+      },
+      blur_cards: {
+        up: "badminton",
+        down: "swimming",
+      },
     },
     swimming: {
       color: "#0479CE",
       h1: "SPLASH INTO WELLNESS",
       p: `Dive into the world of swimming, a beloved water-based activity that offers a full-body workout through arm and leg movements. Whether for leisure or competition, swimming engages muscles throughout the body, providing a low-impact exercise option. Our facility boasts a semi-heated indoor swimming pool with 25 meters and 5 lanes, perfect for leisurely swims or learn-to-swim programs. Additionally, it serves as an excellent venue for athlete warm-ups, cool-downs, and rehabilitation exercises. Jump in and make a splash!`,
-      img1: "/swim-img1.jpg",
-      img2: "/swim-img2.jpg",
-      img3: "/swim-img3.jpg",
-      svg1: "/swim-small.svg",
-      svg1_width: 100.06,
-      svg1_pos: "right-[-2.5rem] top-[2rem]",
       h2: "SWIMMING",
-      svg2: "/poolh.svg",
+      img: {
+        1: "/swim-img1.jpg",
+        2: "/swim-img2.jpg",
+        3: "/swim-img3.jpg",
+      },
+      svg: {
+        1: {
+          img: "/swim-small.svg",
+          width: 100.06,
+          position: "right-[-2.5rem] top-[2rem]",
+        },
+        2: {
+          img: "/poolh.svg",
+        },
+      },
+      blur_cards: {
+        up: "gym",
+        down: "squash",
+      },
     },
     squash: {
       color: "#FB491D",
       h1: "MASTER THE SQUASH",
       p: `Squash is a popular racquet sport that is played by two or four players in a four-walled court with a small, hollow rubber ball. The objective of the game is to hit the ball against the front wall, making it difficult for the opponent to return the ball. Three international-standard wooden courts Capable of hosting state and national tournaments A viewing gallery with seating for 50 persons`,
-      img1: "/squash-img1.jpg",
-      img2: "/squash-img2.jpg",
-      img3: "/squash-img3.jpg",
-      svg1: "/squash-small.svg",
-      svg1_width: 100.06,
-      svg1_pos: "right-[-2rem] top-[-0.4rem]",
       h2: "SQUASH",
-      svg2: "/squash-mini.svg",
+      img: {
+        1: "/squash-img1.jpg",
+        2: "/squash-img2.jpg",
+        3: "/squash-img3.jpg",
+      },
+      svg: {
+        1: {
+          img: "/squash-small.svg",
+          width: 100.06,
+          position: "right-[-2rem] top-[-0.4rem]",
+        },
+        2: {
+          img: "/squash-mini.svg",
+        },
+      },
+      blur_cards: {
+        up: "swimming",
+        down: "badminton",
+      },
     },
   };
 
@@ -69,7 +117,10 @@ function Page() {
     >
       <div className=" absolute top-[38%]">
         <div
-          className=" relative bg-white w-[50.63px] h-[50.63px] rounded-full mb-4 ml-12 cursor-pointer"
+          className=" relative w-[50.63px] h-[50.63px] rounded-full mb-4 ml-12 cursor-pointer "
+          style={{
+            backgroundColor: select !== "badminton" ? "#c1e5e9" : "#FFFFFF",
+          }}
           onClick={() => setSelect("badminton")}
         >
           <Image
@@ -81,7 +132,10 @@ function Page() {
           />
         </div>
         <div
-          className=" relative bg-white w-[50.63px] h-[50.63px] rounded-full mb-4 ml-12 cursor-pointer"
+          className=" relative w-[50.63px] h-[50.63px] rounded-full mb-4 ml-12 cursor-pointer "
+          style={{
+            backgroundColor: select !== "gym" ? "#c1e5e9" : "#FFFFFF",
+          }}
           onClick={() => setSelect("gym")}
         >
           <Image
@@ -93,7 +147,10 @@ function Page() {
           />
         </div>
         <div
-          className=" relative bg-white w-[50.63px] h-[50.63px] rounded-full mb-4 ml-12 cursor-pointer"
+          className=" relative w-[50.63px] h-[50.63px] rounded-full mb-4 ml-12 cursor-pointer "
+          style={{
+            backgroundColor: select !== "swimming" ? "#c1e5e9" : "#FFFFFF",
+          }}
           onClick={() => setSelect("swimming")}
         >
           <Image
@@ -105,7 +162,10 @@ function Page() {
           />
         </div>
         <div
-          className=" relative bg-white w-[50.63px] h-[50.63px] rounded-full mb-4 ml-12 cursor-pointer"
+          className=" relative w-[50.63px] h-[50.63px] rounded-full mb-4 ml-12 cursor-pointer "
+          style={{
+            backgroundColor: select !== "squash" ? "#c1e5e9" : "#FFFFFF",
+          }}
           onClick={() => setSelect("squash")}
         >
           <Image
@@ -117,7 +177,7 @@ function Page() {
           />
         </div>
       </div>
-      <Template data={data[select]} />
+      <Template data={data[select]} all_data={data} />
     </section>
   );
 }
